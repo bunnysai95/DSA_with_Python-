0c9e3fa56ff00433038749a59b01e58e167e21cb
bunnysai95 <matetisaipranay95@gmail.com>
2026-03-31 23:48:30 -0400

    commit_3/31/26

--- sliding_window/48_leetcode.py
+++ sliding_window/48_leetcode.py
@@ -1,3 +1,7 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
 strs = ["eat","tea","tan","ate","nat","bat"]
 
 from collections import defaultdict
